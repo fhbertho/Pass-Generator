8171798838c6e0fe8bdab85c502b9ef8780ccd09
fhbertho <fhbertho@gmail.com>
2023-04-26 08:00:51 -0400

    alteracao tirando a criptografia

--- pass_gen.py
+++ pass_gen.py
@@ -1,69 +1,26 @@
-import hashlib
-import getpass
-import json
+import random
+import string
 
-# Definir o nome do arquivo que irá armazenar as senhas
-PASSWORD_FILE = 'passwords.json'
+# Definir uma função para gerar uma senha forte
+def generate_password(length=12):
+    # Define os caracteres que podem ser usados na senha
+    chars = string.ascii_letters + string.digits + string.punctuation
 
-# Pedir ao usuário para criar uma senha mestra
-master_password = getpass.getpass("Digite a senha mestra: ")
+    # Gera uma senha aleatória usando os caracteres definidos
+    password = ''.join(random.choice(chars) for _ in range(length))
 
-# Criar um hash da senha mestra usando a função de hash SHA256
-master_hash = hashlib.sha256(master_password.encode()).hexdigest()
+    return password
 
-# Definir as funções para criptografar e descriptografar as senhas
-def encrypt(password, key):
-    cipher = []
-    for i in range(len(password)):
-        key_c = key[i % len(key)]
-        cipher_c = chr((ord(password[i]) + ord(key_c)) % 256)
-        cipher.append(cipher_c)
-    return ''.join(cipher)
+# Pedir ao usuário quantas senhas eles desejam gerar
+num_passwords = int(input("Quantas senhas deseja gerar? "))
 
-def decrypt(cipher, key):
-    password = []
-    for i in range(len(cipher)):
-        key_c = key[i % len(key)]
-        password_c = chr((ord(cipher[i]) - ord(key_c)) % 256)
-        password.append(password_c)
-    return ''.join(password)
+# Pedir ao usuário o tamanho das senhas que eles desejam gerar
+password_length = int(input("Qual o tamanho das senhas que deseja gerar? "))
 
-# Definir as funções para adicionar e recuperar senhas
-def add_password():
-    website = input("Digite o nome do website: ")
-    username = input("Digite o nome de usuário: ")
-    password = getpass.getpass("Digite a senha: ")
-    encrypted_password = encrypt(password, master_hash)
-    with open(PASSWORD_FILE, 'r') as f:
-        passwords = json.load(f)
-    passwords[website] = {'username': username, 'password': encrypted_password}
-    with open(PASSWORD_FILE, 'w') as f:
-        json.dump(passwords, f)
-
-def get_password():
-    website = input("Digite o nome do website: ")
-    with open(PASSWORD_FILE, 'r') as f:
-        passwords = json.load(f)
-    if website in passwords:
-        username = passwords[website]['username']
-        encrypted_password = passwords[website]['password']
-        password = decrypt(encrypted_password, master_hash)
-        print(f"Nome de usuário: {username}")
-        print(f"Senha: {password}")
-    else:
-        print("Website não encontrado")
-
-# Definir o loop principal do programa
-while True:
-    print("\n1 - Adicionar senha")
-    print("2 - Recuperar senha")
-    print("3 - Sair")
-    choice = input("Escolha uma opção: ")
-    if choice == '1':
-        add_password()
-    elif choice == '2':
-        get_password()
-    elif choice == '3':
-        break
-    else:
-        print("Opção inválida")
+# Gerar as senhas e armazená-las em um arquivo de texto
+with open("/home/kali/Desktop/cyber_projects/senhas.txt", "a") as f:
+    for i in range(num_passwords):
+        service_name = input(f"Para qual serviço deseja gerar a senha {i+1}? ")
+        password = generate_password(password_length)
+        f.write(f"{service_name}: {password}\n")
+        print(f"Senha {i+1} para {service_name}: {password}")
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 Password Generator v1.0
 
-Administre e gere senhas seguras.
+Crie senhas seguras para suas contas na web, sem precisar ter que memoriza-las ou fazer anotações sempre que criar uma nova senha.
+
+Utilizando o gerador de senhas automáticas, após a criação da senha forte, ela automaticamente é salva em um arquivo de senha criptografado.
+
+Caso você esqueça a senha, basta acessar o arquivo criptografado e lá você conseguirá ver todas as suas senhas criadas.
+
+
